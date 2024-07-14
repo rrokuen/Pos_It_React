7f730240b1db6e7b47356da7721cd8e48f4f5e4d
rrokuen <rokuen8@gmail.com>
2024-07-13 23:03:00 -0400

    Función Agregar Post-It

--- post_it_proyect/src/componentes/MyPost_It.jsx
+++ post_it_proyect/src/componentes/MyPost_It.jsx
@@ -1,22 +1,43 @@
 import { PostIt } from "./Post_It";
 import { useRef, useState } from "react";
 import { v4 as uuid } from 'uuid';
+
 export function MyPostIt() {
+
     const tituloRef = useRef();
     const descripcionRef = useRef();
-    const importante = useRef();
-    
+    const importanciaRef = useRef();
+
+    const [postList, setPostList] = useState([
+        {id: uuid(), titulo: "Titulo no Importante", descripcion: "Descripción no Importante", importante: false},
+        {id: uuid(), titulo: "Titulo Importante", descripcion: "Descripción Importante", importante: true}
+    ])  
+
+    function addPost() {
+        console.log("Presionando boton agregar");
+        const titulo = tituloRef.current.value;
+        const descripcion = descripcionRef.current.value;
+        const importancia = importanciaRef.current.checked;
+        console.log(`Se escribió ${titulo} ${descripcion} ${importancia}`);
+
+        if(titulo.trim() === ''){
+            alert('Campos vacíos');
+            
+            return;
+        }
+    }
+
     return (
         <body>
             <header>
                 <div class="contenedor_header">
                     <h1>Post It Simultator!</h1>
                     <div class="inpts_header">
-                        <input type="text" placeholder="Título" id="inptTitulo"></input>
-                        <input type="text" placeholder="Descripción" id="inptDescripcion"></input>
-                        <input type="checkbox" id="inptCheckbox"></input>
+                        <input ref={tituloRef} type="text" placeholder="Título" id="inptTitulo"></input>
+                        <input ref={descripcionRef} type="text" placeholder="Descripción" id="inptDescripcion"></input>
+                        <input ref={importanciaRef} type="checkbox" id="inptCheckbox"></input>
                         <label>Importante!</label>
-                        <button class="btnAgg">Agregar</button>
+                        <button onClick={addPost} class="btnAgg">Agregar</button>
                     </div>
                 </div>
             </header>
